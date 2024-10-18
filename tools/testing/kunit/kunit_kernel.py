8440410283bb5533b676574211f31f030a18011b
Jason Gunthorpe <jgg@nvidia.com>
2024-10-18 13:28:40 -0300

    kunit: tool: catch warnings generated by the kernel
    If the kunit being run generates a WARN for some reason kunit.py
    ignores it and declares the tested PASSED. This is very much not
    desirable, as tests that are hitting WARN's are probably actually
    failing.
    
    Take the simple approach to reducing this by setting panic_on_warn when
    running the kernel. The kernel crashes and kunit.py shows the WARN and
    reports the test fails.
    
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- tools/testing/kunit/kunit_kernel.py
+++ tools/testing/kunit/kunit_kernel.py
@@ -355,6 +355,8 @@ class LinuxSourceTree:
 		if filter_action:
 			args.append('kunit.filter_action=' + filter_action)
 		args.append('kunit.enable=1')
+		args.append('panic_on_warn=1')
+		args.append('panic=-1')
 
 		process = self._ops.start(args, build_dir)
 		assert process.stdout is not None  # tell mypy it's set
