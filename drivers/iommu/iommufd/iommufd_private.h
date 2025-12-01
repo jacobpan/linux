7f07d5d7faa3a3c302789d2ce9666fb3a5c4801f
Jacob Pan <jacob.pan@linux.microsoft.com>
2025-12-01 09:30:12 -0800

    iommufd: Add an ioctl IOMMU_IOAS_GET_PA to query PA from IOVA
    To support no-IOMMU mode where userspace drivers perform unsafe DMA
    using physical addresses, introduce a new API to retrieve the
    physical address of a user-allocated DMA buffer that has been mapped to
    an IOVA via IOAS. The mapping is backed by mock I/O page tables maintained
    by generic IOMMUPT framework.
    
    Suggested-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Jacob Pan <jacob.pan@linux.microsoft.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    ---
    v2:
       - Scan the contiguous physical-address span beyond the first page and return its length.

--- drivers/iommu/iommufd/iommufd_private.h
+++ drivers/iommu/iommufd/iommufd_private.h
@@ -118,6 +118,8 @@ int iopt_map_pages(struct io_pagetable *iopt, struct list_head *pages_list,
 int iopt_unmap_iova(struct io_pagetable *iopt, unsigned long iova,
 		    unsigned long length, unsigned long *unmapped);
 int iopt_unmap_all(struct io_pagetable *iopt, unsigned long *unmapped);
+int iopt_get_phys(struct io_pagetable *iopt, unsigned long iova, u64 *paddr,
+		  u64 *length);
 
 int iopt_read_and_clear_dirty_data(struct io_pagetable *iopt,
 				   struct iommu_domain *domain,
@@ -346,6 +348,7 @@ int iommufd_ioas_map_file(struct iommufd_ucmd *ucmd);
 int iommufd_ioas_change_process(struct iommufd_ucmd *ucmd);
 int iommufd_ioas_copy(struct iommufd_ucmd *ucmd);
 int iommufd_ioas_unmap(struct iommufd_ucmd *ucmd);
+int iommufd_ioas_get_pa(struct iommufd_ucmd *ucmd);
 int iommufd_ioas_option(struct iommufd_ucmd *ucmd);
 int iommufd_option_rlimit_mode(struct iommu_option *cmd,
 			       struct iommufd_ctx *ictx);
