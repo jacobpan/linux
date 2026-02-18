e8109e2fbc5dd0dace713258afbf0cdf459968bc
Jacob Pan <jacob.pan@linux.microsoft.com>
2026-02-18 19:22:59 +0000

    vfio: Allow null group for noiommu without containers
    In case of noiommu mode is enabled for VFIO cdev without VFIO container
    nor IOMMUFD provided compatibility container, there is no need to
    create a dummy group. Update the group operations to tolerate null group
    pointer.
    
    Signed-off-by: Jacob Pan <jacob.pan@linux.microsoft.com>

--- drivers/vfio/group.c
+++ drivers/vfio/group.c
@@ -390,6 +390,9 @@ int vfio_device_block_group(struct vfio_device *device)
 	struct vfio_group *group = device->group;
 	int ret = 0;
 
+	if (vfio_null_group_allowed() && !group)
+		return 0;
+
 	mutex_lock(&group->group_lock);
 	if (group->opened_file) {
 		ret = -EBUSY;
@@ -406,6 +409,9 @@ out_unlock:
 void vfio_device_unblock_group(struct vfio_device *device)
 {
 	struct vfio_group *group = device->group;
+
+	if (vfio_null_group_allowed() && !group)
+		return;
 
 	mutex_lock(&group->group_lock);
 	group->cdev_device_open_cnt--;
@@ -597,6 +603,14 @@ static struct vfio_group *vfio_noiommu_group_alloc(struct device *dev,
 	struct iommu_group *iommu_group;
 	struct vfio_group *group;
 	int ret;
+
+	/*
+	 * With noiommu enabled under cdev interface only, there is no need to
+	 * create a vfio_group if the group based containers are not enabled.
+	 * The cdev interface is exclusively used for iommufd.
+	 */
+	if (vfio_null_group_allowed())
+		return NULL;
 
 	iommu_group = iommu_group_alloc();
 	if (IS_ERR(iommu_group))
