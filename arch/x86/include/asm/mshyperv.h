9a3a65a534d8533a150344cd218096c3621db749
Mukesh R <mrathor@linux.microsoft.com>
2026-05-11 19:02:56 -0700

    PCI: hv: VMBus and PCI device IDs for PCI passthru
    On Hyper-V, most hypercalls related to PCI passthru to map/unmap regions,
    interrupts, etc need a device ID as a parameter. This device ID refers
    to that specific device during the lifetime of passthru.
    
    An L1VH VM only contains VMBus based devices. A device ID for a VMBus
    device is slightly different in that it uses the hv_pcibus_device info
    for building it to make sure it matches exactly what the hypervisor
    expects. This VMBus based device ID is needed when attaching devices in
    an L1VH based guest VM. Add a function to build and export it. Before
    building it, a check is done to make sure the device is a valid VMBus
    device.
    
    In remaining cases, PCI device ID is used. So, also make PCI device ID
    build function hv_build_devid_type_pci() public.
    
    Signed-off-by: Mukesh R <mrathor@linux.microsoft.com>

--- arch/x86/include/asm/mshyperv.h
+++ arch/x86/include/asm/mshyperv.h
@@ -248,6 +248,12 @@ void hv_crash_asm_end(void);
 static inline void hv_root_crash_init(void) {}
 #endif  /* CONFIG_MSHV_ROOT && CONFIG_CRASH_DUMP */
 
+#if IS_ENABLED(CONFIG_HYPERV_IOMMU)
+u64 hv_build_devid_type_pci(struct pci_dev *pdev);
+#else
+static inline u64 hv_build_devid_type_pci(struct pci_dev *pdev) { return 0; }
+#endif /* IS_ENABLED(CONFIG_HYPERV_IOMMU) */
+
 #else /* CONFIG_HYPERV */
 static inline void hyperv_init(void) {}
 static inline void hyperv_setup_mmu_ops(void) {}
