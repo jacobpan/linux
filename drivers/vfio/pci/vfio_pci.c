8ffca1e2bf17c8ef9d36bdc08d234b04995a0129
Jacob Pan <jacob.pan@linux.microsoft.com>
2026-09-03 09:04:16 -0700

    TESTING ONLY: vfio/pci: Allow blocked Intel accelerators
    Temporarily allow the Intel QAT, DSA, and IAX devices that are normally
    denied by vfio-pci so the local external attach prototype can exercise
    those devices.

--- drivers/vfio/pci/vfio_pci.c
+++ drivers/vfio/pci/vfio_pci.c
@@ -73,7 +73,7 @@ static bool vfio_pci_dev_in_denylist(struct pci_dev *pdev)
 		case PCI_DEVICE_ID_INTEL_QAT_DH895XCC_VF:
 		case PCI_DEVICE_ID_INTEL_DSA_SPR0:
 		case PCI_DEVICE_ID_INTEL_IAX_SPR0:
-			return true;
+			return false;
 		default:
 			return false;
 		}
